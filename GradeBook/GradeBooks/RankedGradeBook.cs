6cae91657305cfb4668ba73c03e6fa0f6d0ace50
Troy Campbell <campbell@mmsi.com>
2020-09-16 13:32:40 +1000

    Override RankedGradeBook's GetLetterGrade

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -11,5 +11,45 @@ namespace GradeBook.GradeBooks
         {
             Type = GradeBookType.Ranked;
         }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if(Students.Count < 5)
+            {
+                throw new InvalidOperationException();
+            }
+
+            int x = (Students.Count / 5);
+
+            List<double> studentScores = new List<double>();
+            foreach(Student s in this.Students)
+            {
+                studentScores.Add(s.AverageGrade);
+            }
+            studentScores.Sort();
+            studentScores.Reverse();
+
+            for(int g = 0; g < studentScores.Count; ++g)
+            {
+                if(studentScores[g] <= averageGrade)
+                {
+                    if(g < x)
+                    {
+                        return 'A';
+                    } else if (g < (2*x))
+                    {
+                        return 'B';
+                    } else if (g < (3*x))
+                    {
+                        return 'C';
+                    } else if (g < (4*x))
+                    {
+                        return 'D';
+                    }
+                }
+            }
+
+            return 'F';
+        }
     }
 }
